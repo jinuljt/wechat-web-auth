23f673dab04038a2e8f33b7a70c8820a6ea18988
Juntao Liu <jinuljt@gmail.com>
2015-07-28 21:49:40 +0800

    重新整理了目录结构，增加了setup.py 文件

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="wechat-web-auth",
+    version="0.1.0",
+    keywords=("wechat", "web auth"),
+    description=u"微信公众平台网页授权获取用户信息python封装",
+    long_description=u"微信公众平台网页授权获取用户信息python封装",
+    license="MIT License",
+
+    url="https://github.com/jinuljt/wechat-web-auth",
+    author="Juntao Liu",
+    author_email="jinuljt@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=map(lambda x: x.replace('==', '>='), open("requirements.txt").readlines()),
+    )
